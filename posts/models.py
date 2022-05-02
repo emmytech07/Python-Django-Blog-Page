e04f110c67658ccaf15d855930f21dd3a6e55c17
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-05-02 15:28:42 +0100

    Deploying Simple Blog page

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from turtle import title
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=10000)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
